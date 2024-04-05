06a602b50fdc4efe292a0d184046fc9508ab01e9
IweZix <IweZix@users.noreply.github.com>
2024-04-05 18:12:03 +0200

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -10,14 +10,18 @@ let jsonDbPath = __dirname + '/../logs/';
  * @param {Error} error 
  */
 function writeErrorCommande(interaction, error) {
-  const path = getPath(interaction.guildId);
-  const data = parse(path, []);
-  data.push({
-    date: new Date().toISOString(),
-    command: interaction.commandName,
-    error: error.message
-  });
-  serialize(path, data);
+  try {
+    const path = getPath(interaction.guildId);
+    const data = parse(path, []);
+    data.push({
+      date: new Date().toISOString(),
+      command: interaction.commandName,
+      error: error.message
+    });
+    serialize(path, data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 /**
@@ -26,13 +30,17 @@ function writeErrorCommande(interaction, error) {
  * @returns the path to the json file
  */
 function getPath(guildId) {
-  const date = new Date();
-  const dateString = date.toISOString().split('T')[0];
-  const path = jsonDbPath + dateString + '/' + guildId +  '.json';
-  if (!fs.existsSync(jsonDbPath)) {
-    fs.mkdirSync(jsonDbPath + dateString);
+  try {
+    const date = new Date();
+    const dateString = date.toISOString().split('T')[0];
+    const path = jsonDbPath + dateString + '/' + guildId +  '.json';
+    if (!fs.existsSync(jsonDbPath)) {
+      fs.mkdirSync(jsonDbPath + dateString);
+    }
+    return path;
+  } catch (error) {
+    console.error(error);
   }
-  return path;
 }
 
 module.exports = {
